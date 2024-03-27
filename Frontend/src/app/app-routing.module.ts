2a22865830d069d7ff91957b4829eae833f8a727
abdul.shakoor@systemsltd.com <abdul.shakoor@systemsltd.com>
2024-03-28 00:24:01 +0500

    subtask changes

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EditTaskComponent } from './shared/edit-task/edit-task.component';
-import { TaskResolver } from './resolvers/task-resolver.service';
 
 const routes: Routes = [
   { path:'', component:HomeComponent },
   { path: 'edit', redirectTo: '', pathMatch: 'full'},
-  { path:'edit/:id', component:EditTaskComponent, resolve: {task: TaskResolver} },
+  { path:'edit/:id', component:EditTaskComponent },
   { path: ':id', component: HomeComponent},
   { path: 'create', component: EditTaskComponent },  
   { path: '**', redirectTo:'', pathMatch:'full' }
