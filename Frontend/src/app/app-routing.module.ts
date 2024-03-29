b9defc85d83ef86d253d9adc992b8621128c80d3
abdul.shakoor@systemsltd.com <abdul.shakoor@systemsltd.com>
2024-03-29 15:21:07 +0500

    tags completed

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,14 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { EditTaskComponent } from './shared/edit-task/edit-task.component';
 import { UserComponent } from './user/user.component';
 import { TagsComponent } from './tags/tags.component';
+import { EditTagComponent } from './tags/edit-tag/edit-tag.component';
+import { TagResolver } from './resolvers/tag-resolver.service'
 
 const routes: Routes = [
   { path:'', component:HomeComponent },
   { path: 'user', component:UserComponent, pathMatch:'full'},  
-  { path: 'tags', component:TagsComponent, pathMatch:'full'},  
+  { path: 'tags', component:TagsComponent },
+  { path: 'tags/create', component:EditTagComponent},
+  { path: 'tags/edit/:id', component:EditTagComponent, resolve:{tag: TagResolver}},
   { path: 'create', component: EditTaskComponent },  
   { path: 'edit', redirectTo: '', pathMatch: 'full'},
-  { path:'edit/:id', component:EditTaskComponent },
+  { path: 'edit/:id', component:EditTaskComponent },
   { path: ':id', component: HomeComponent},  
   { path: '**', redirectTo:'', pathMatch:'full' }
 ];
--- Frontend/src/app/tags/tags.component.ts
+++ Frontend/src/app/tags/tags.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Tag } from '../model/Tag';
 import { TagService } from '../service/tag.service';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tags',
@@ -12,17 +13,24 @@ export class TagsComponent implements OnInit {
 
   tags: Tag[] = [];
   private tasksSubscription!: Subscription;
-  
-  constructor(private tagService: TagService) { }
+
+  constructor(private tagService: TagService, private router:Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.tags = this.tagService.tags;
-    this.tagService.tagSubject.subscribe(tags => this.tags = tags);    
+    this.tasksSubscription = this.tagService.tagSubject.subscribe(tags => this.tags = tags);
   }
 
   deleteTag(id: number) {
     this.tagService.deleteTag(+id);
   }
 
+  EditTag(id: number) {    
+    this.router.navigate(['/tags/edit', id], { relativeTo: this.route });
+  }    
+
+  ngDestroy() {
+    this.tasksSubscription.unsubscribe();
+  } 
 
 }
